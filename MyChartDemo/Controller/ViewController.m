5048831e540f6aa9c989e566506a44973ac86ea2
Ahri <hung.mighty.nguyen@gmail.com>
2017-07-12 16:32:50 +0700

    Fix Navbar overlap barChart Uiview

--- MyChartDemo/Controller/ViewController.m
+++ MyChartDemo/Controller/ViewController.m
@@ -17,7 +17,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.navigationController.navigationBar.translucent = NO;
 }
 
 - (void)viewWillAppear:(BOOL)animated {
